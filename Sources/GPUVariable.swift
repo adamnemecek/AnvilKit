f81e3d5c03372a3b3728723d3404a55f181665dd
Adam Nemecek <adamnemecek@gmail.com>
2017-09-10 13:49:25 -0700

    gpuvariable

---
+++ Sources/GPUVariable.swift
@@ -0,0 +1,56 @@
+//
+//  GPUVariable.swift
+//  AnvilKit
+//
+//  Created by Adam Nemecek on 9/10/17.
+//
+
+import MetalKit
+
+final class GPUVariable<T> {
+    private(set) var buffer : MTLBuffer
+
+    init(gpu: MTLDevice, value : T) {
+        var v = value
+
+        buffer = gpu.makeBuffer(bytes: &v,
+                                length: MemoryLayout<T>.size,
+                                options: [])!
+    }
+
+    deinit {
+        buffer.setPurgeableState(.empty)
+    }
+
+    var size : Int {
+        @inline(__always)
+        get {
+            return MemoryLayout<T>.size
+        }
+    }
+
+    var value : T {
+        @inline(__always)
+        get {
+            return buffer.contents().assumingMemoryBound(to: T.self).pointee
+        }
+
+        @inline(__always)
+        set {
+            var v = newValue
+            memcpy(buffer.contents(), &v, size)
+        }
+    }
+}
+
+extension MTLComputeCommandEncoder {
+    func setVariable<T>(_ variable : GPUVariable<T>, index : Int) {
+        setBuffer(variable.buffer, offset : 0, index: index)
+    }
+}
+
+extension MTLRenderCommandEncoder {
+    func setVariable<T>(_ variable : GPUVariable<T>, index : Int) {
+        setVertexBuffer(variable.buffer, offset : 0, index: index)
+    }
+}
