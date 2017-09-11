2f588a86e535d79228eacd1e7e79febd75f1cf44
Adam Nemecek <adamnemecek@gmail.com>
2017-09-11 09:04:55 -0700

    * gpudevice

--- Sources/GPUDevice.swift
+++ Sources/GPUDevice.swift
@@ -15,39 +15,30 @@ struct RenderConfig {
 }
 
 final class GPUDevice {
-    // todo private
+
     let device : MTLDevice = MTLCreateSystemDefaultDevice()!
     let library : MTLLibrary
     let commandQueue: MTLCommandQueue
-
-    //    private let indexBuffer : MTLBuffer
 
     static let shared = GPUDevice()
 
     private init() {
         library = device.makeDefaultLibrary()!
         commandQueue = device.makeCommandQueue()!
-        var buf: [UInt16] = (0..<2<<16).map { $0 }
-
-
-        //        indexBuffer = device.makeBuffer(bytes: &buf, length: buf.count, options: [])!
     }
 
-    func renderPipeline(for config: RenderConfig) -> MTLRenderPipelineDescriptor {
-        let v = library.makeFunction(name: config.vertex)
-        let f = library.makeFunction(name: config.fragment)
+    func renderPipeline(vertex: String, frag : String, pixelFormat : MTLPixelFormat = .default) -> MTLRenderPipelineDescriptor {
+        let v = library.makeFunction(name: vertex)
+        let f = library.makeFunction(name: frag)
         let d = MTLRenderPipelineDescriptor()
         d.vertexFunction = v
         d.fragmentFunction = f
-        d.colorAttachments[0].pixelFormat = config.pixelFormat
+        d.colorAttachments[0].pixelFormat = pixelFormat
 
         d.colorAttachments[0].isBlendingEnabled = true
         d.colorAttachments[0].rgbBlendOperation = .add
         d.colorAttachments[0].alphaBlendOperation = .add
 
-
-        // I don't believe this but this is what it is...
-        //        d.colorAttachments[0].
         d.colorAttachments[0].sourceRGBBlendFactor = .one
         d.colorAttachments[0].sourceAlphaBlendFactor = .one
         d.colorAttachments[0].destinationRGBBlendFactor = .oneMinusSourceAlpha
@@ -59,15 +50,7 @@ final class GPUDevice {
     func computePipeline(name : String) -> MTLComputePipelineState {
         let f = library.makeFunction(name: name)!
         return try! device.makeComputePipelineState(function: f)
-
     }
-
-    //    func makeCommandQueue() -> MTLCommandQueue {
-    //        guard let queue = device.makeCommandQueue() else {
-    //            fatalError("failed on mtl queue")
-    //        }
-    //        return queue
-    //    }
 
     func makeBuffer<T>(for content: [T]) -> MTLBuffer {
         var cpy = content
