0a3402b1ffa581bf6edf16ae054ab460340207b1
Adam Nemecek <adamnemecek@gmail.com>
2017-09-11 11:59:21 -0700

    * gpuarray

--- Sources/GPUArray.swift
+++ Sources/GPUArray.swift
@@ -7,25 +7,91 @@
 
 import MetalKit
 
+extension UnsafeMutablePointer {
+    @inline(__always)
+    init(mtlBuffer : MTLBuffer) {
+        self.init(OpaquePointer(mtlBuffer.contents()))
+    }
+}
+
+extension UnsafeMutableBufferPointer {
+    @inline(__always)
+    init(mtlBuffer : MTLBuffer) {
+        let count = mtlBuffer.length / MemoryLayout<Element>.size
+
+        self.init(start: .init(mtlBuffer : mtlBuffer), count: count)
+
+    }
+}
+
+struct MetalStorage<Element> : MutableCollection {
+    typealias Index = Int
+
+    private var content : MTLBuffer
+
+    private(set) var count : Int
+
+    init(capacity : Int) {
+        count = 0
+        fatalError()
+    }
+
+
+    var capacity : Int {
+        return content.length / MemoryLayout<Element>.size
+    }
+
+    //private let content : [Element]
+
+    var startIndex : Index {
+        fatalError()
+    }
+
+    var endIndex : Index {
+        fatalError()
+    }
+
+    private var ptr : UnsafeMutableBufferPointer<Element> {
+        @inline(__always)
+        get {
+            return .init(mtlBuffer: content)
+        }
+    }
+
+    subscript(index: Index) -> Element {
+        get {
+            return ptr[index]
+        }
+        set {
+            ptr[index] = newValue
+        }
+    }
+
+    func index(after i: Index) -> Index {
+        return i + 1
+    }
+}
 
 public final class GPUArray<Element> : RangeReplaceableCollection, RandomAccessCollection {
     public typealias Index = Int
+    private var content : MetalStorage<Element>
 
     public init() {
         fatalError()
     }
+
     //private let content : [Element]
 
     public var startIndex : Index {
-        fatalError()
+        return 0
     }
 
     public var count : IndexDistance {
-        fatalError()
+        return content.count
     }
 
     public var endIndex : Index {
-        fatalError()
+        return content.endIndex
     }
 
     public subscript(index: Index) -> Element {
